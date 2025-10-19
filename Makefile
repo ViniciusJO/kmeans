a59963e1e65b2f893c4e4494237da29285d56178
ViniciusJO <viniciusjosedeoliveira@outlook.com>
2025-10-19 09:29:54 -0300

    start port to zig build system

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
-all:
-	zig build-exe src/main.zig stb_image.o stb_image_write.o stb_image_resize2.o -lc -femit-bin='color_juicer' --color on
+all: color_juicer
+	
+color_juicer: zig-out/stbi.zig zig-out/stbiw.zig zig-out/stbir.zig
+	zig \
+		build-exe \
+		src/main.zig \
+		stb_image.o \
+		stb_image_write.o \
+		stb_image_resize2.o \
+		-lc \
+		-femit-bin='color_juicer' \
+		--color on
+zig-out/stbi.zig: src/stb/stb_image.h
+	zig translate-c -I /usr/include -DSTB_IMAGE_IMPLEMENTATION $< > $@
+zig-out/stbiw.zig: src/stb/stb_image_write.h
+	zig translate-c -I /usr/include -DSTB_IMAGE_WRITE_IMPLEMENTATION $< > $@
+zig-out/stbir: src/stb/stb_image_resize.h
+	zig translate-c -I /usr/include -DSTB_IMAGE_RESIZE_IMPLEMENTATION $< > $@
--- build.zig
+++ build.zig
@@ -4,11 +4,55 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "kmeans_",
+    const kmeans = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const stb_image = b.addTranslateC(.{
+        .target = target,
+        .root_source_file = b.path("src/stb/stb_image.h"),
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    const stb_image_write = b.addTranslateC(.{
+        .target = target,
+        .root_source_file = b.path("src/stb/stb_image_write.h"),
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    const stb_image_resize = b.addTranslateC(.{
+        .target = target,
+        .root_source_file = b.path("src/stb/stb_image_resize2.h"),
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    kmeans.addImport("stbi", stb_image.createModule());
+    kmeans.addImport("stbiw", stb_image_write.createModule());
+    kmeans.addImport("stbir", stb_image_resize.createModule());
+    
+    kmeans.addCSourceFiles(.{
+        .files = &[_][]const u8 {
+            "src/stb/stb_image.h",
+            "src/stb/stb_image_resize2.h",
+            "src/stb/stb_image_write.h",
+        },
+        .flags = &[_][]const u8 {
+            "-g",
+            "-DSTB_IMAGE_IMPLEMENTATION",
+            "-DSTB_IMAGE_WRITE_IMPLEMENTATION",
+            "-DSTB_IMAGE_RESIZE_IMPLEMENTATION",
+        },
+        .language = .c
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "color_juicer",
+        .root_module = kmeans,
     });
 
     b.installArtifact(exe);
@@ -23,28 +67,19 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    // const exe_unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    //
+    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_exe_unit_tests.step);
 
     // const zigimg_dependency = b.dependency("zigimg", .{
     //     .target = target,
